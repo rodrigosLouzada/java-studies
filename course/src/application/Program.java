039d3e69c61ec5a3c8488bd170fadb2169628192
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2024-12-13 17:54:47 -0300

    class and objs, primitive way

--- course/src/application/Program.java
+++ course/src/application/Program.java
@@ -1,10 +1,26 @@
 package application;
 
+import java.util.Scanner;
+
+import entities.Triangle;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner scan = new Scanner(System.in);
+		Triangle x, y, z;
+		x = new Triangle();
+		y = new Triangle();
+		
+		System.out.println("quais as medidas?");
+		 x.a = scan.nextDouble();
+		 x.b = scan.nextDouble();
+		 x.c = scan.nextDouble();
+		 double total = 0.0;
+		 total += x.a + x.b + x.c ;
+		 
+		 System.out.println(" a soma dos lados é: " + total);
 	}
 
 }
