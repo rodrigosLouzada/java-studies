f620e8f4856ecd7f96aa1610033c688f93f8305f
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2024-12-20 18:19:22 -0300

    class and obj, same thing

--- course/src/application/Program_3.java
+++ course/src/application/Program_3.java
@@ -1,29 +1,39 @@
 package application;
 
-import java.lang.invoke.VolatileCallSite;
 import java.util.Scanner;
+
+import util.Calculator;
 
 public class Program_3 {
 	
-	public static final double PI = 3.14;
+	//public static final double PI = 3.14;
 
+	
 	public static void main(String[] args) {
 
 		Scanner scan = new Scanner(System.in);
+		Calculator cal = new Calculator();
 		
 		System.out.println("digite um raio para o resultado");
 		double raio = scan.nextDouble();
 		
+		double circu = cal.circunferencia(raio);
+		double vol = cal.vol(raio);
 		
+		System.out.println("valores calculados: " + circu + "  " + vol);
+		
+		/*
 		double circuferencia = circunferencia(raio);
 		double volume = vol(raio);
-		System.out.println("o valor da circuferencia: " + circuferencia + "    o valor do volume: " + volume);
+		*/
+		
+		//System.out.println("o valor da circuferencia: " + circuferencia + "    o valor do volume: " + volume);
 		
 		
 		scan.close();
 	}
 	
-	public static double circunferencia(double raio) {
+/*	public static double circunferencia(double raio) {
 		
 		return 2.0 * PI ;
 	}
@@ -31,5 +41,5 @@ public class Program_3 {
 	public static double vol (double raio) {
 		return 4.0 * PI * raio * raio * raio /3.0;
 	}
-
+*/
 }
