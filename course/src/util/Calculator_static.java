61b75c0b1fab32065df339911708bcc3cad6aca9
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2024-12-23 14:30:51 -0300

    creating static methods

---
+++ course/src/util/Calculator_static.java
@@ -0,0 +1,17 @@
+package util;
+
+public class Calculator_static {
+	
+public static final double PI = 3.14;
+	
+	public static double circunferencia(double raio) {
+		
+		return 2.0 * PI * raio ;
+	}
+	
+	public static double vol (double raio) {
+		return 4.0 * PI * raio * raio * raio /3.0;
+	}
+	
+	
+}
