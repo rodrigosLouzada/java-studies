0b2aab8fcf06fd906f47af9a64bf140aa60af613
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2024-12-27 19:06:04 -0300

    main program, working

--- course/src/application/Client_Program.java
+++ course/src/application/Client_Program.java
@@ -10,19 +10,66 @@ public class Client_Program {
 
 		Scanner scan = new Scanner(System.in);
 		ClientBank novoClienteBank = new ClientBank();
+		Double saldo = 0.0;
+		Double valordeposito ,valorSaque = 0.0;
 		
-		System.out.println("gostaria de realizar um cadastro? Sim ou Não?");
+		System.out.println("gostaria de realizar um cadastro: Sim || Não ?");
 		
-		String respota = scan.next();
+		String resposta = scan.next();
 		
-		if(respota == "Sim") {
-			System.out.println("qual é o seu nome");
+		if(resposta.equals("Sim")){
+			
+			System.out.println("qual é o seu nome?");
+			String clienteNome = scan.next();
+			
+			
+			System.out.println("qual é o número de sua conta?");
+			Integer contaNumero = scan.nextInt();
+			
+			System.out.println("quer realizar um primeiro deposito? Adicone qualquer valor");
+			saldo = scan.nextDouble();
+			
+			novoClienteBank.cadastroConta(contaNumero, clienteNome, saldo);
+			
+			novoClienteBank.mostrarDadosCliente();
+			
+		}else {
+			System.out.println("Para usar o nosso sistema, é necessário um cadastro, tente novamente");
+			return;
 		}
+			
+			
 		
-		
-		
-		
-		
+		do {
+			System.out.println("qual opção quer realizar? : Deposito || Saque");
+			 resposta = scan.next();
+			 
+			 if(resposta.equals("Deposito")) {
+				 System.out.println("de qual valor seria?");
+				 valordeposito = scan.nextDouble();
+				 novoClienteBank.deposito(valordeposito);
+				 
+				 
+				 
+				 
+			 }else if (resposta.equals("Saque")) {
+				 
+				System.out.println("de quanto é o valor do saque");
+				valorSaque = scan.nextDouble();
+				
+				novoClienteBank.saque(valorSaque);
+				
+				
+			}
+			 	
+			 
+			 	System.out.println("quer continuar com as opcóes bancarias : Sim || Não");
+				
+				resposta = scan.next();
+			
+		} while (resposta.equals("Sim"));
+			
+		System.out.println("obrigado, volte sempre!");
 	}
 
 }
