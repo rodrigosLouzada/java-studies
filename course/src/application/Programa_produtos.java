707b540f870d03abdfa0b7fbd42788e0a959767d
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2024-12-30 17:37:49 -0300

    main program for vects of objs, incomplete

---
+++ course/src/application/Programa_produtos.java
@@ -0,0 +1,21 @@
+package application;
+
+import java.util.Scanner;
+
+import entities.Produtos;
+
+public class Programa_produtos {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		
+		int n = scanner.nextInt();
+		
+		Produtos[] Vetorprodutos = new Produtos[n]; 
+		
+		
+		scanner.close();
+	}
+
+}
