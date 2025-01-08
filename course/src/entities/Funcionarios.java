b0b528bb3746c200adf350f11368171b75178c22
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2025-01-08 18:29:46 -0300

    esquelo, incompleto, classe

---
+++ course/src/entities/Funcionarios.java
@@ -0,0 +1,16 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Funcionarios {
+	
+	List<String> nome = new ArrayList<String>();
+	List<Integer> id = new ArrayList<Integer>();
+	List<Double> salario = new ArrayList<Double>();
+	
+
+			
+			
+			
+}
