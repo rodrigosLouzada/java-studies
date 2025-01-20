c2634b669f148ced10ce0b3830686511c61090d8
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2025-01-20 16:05:32 -0300

    valueOF, obj-enum

--- course/src/application/Program_enum.java
+++ course/src/application/Program_enum.java
@@ -1,5 +1,21 @@
 package application;
 
-public class Program_enum {
 
+import java.util.Date;
+
+import entities.Order;
+import entities.enums.OrderStatus;
+
+public class Program_enum {
+	
+	public static void main(String[] args) {
+		
+		Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
+		
+		
+		System.out.println(order);
+		
+		OrderStatus os1 = OrderStatus.DELIVERED;
+		OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+	}
 }
