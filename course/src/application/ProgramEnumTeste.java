d0abf06918b3e4b6d7ecfe1886dd393091dda361
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2025-01-23 17:43:03 -0300

    using for to get another value from enum, creating obj using if, in case of 1 situation or other

--- course/src/application/ProgramEnumTeste.java
+++ course/src/application/ProgramEnumTeste.java
@@ -3,13 +3,14 @@ package application;
 import java.util.Scanner;
 
 import entities.Aluno;
+import entities.enums.AlunoDiversosValores;
 import entities.enums.AlunoEnumSimples;
 
 public class ProgramEnumTeste {
 
 	public static void main(String[] args) {
 
-		System.out.println("digite o seu nome, se está ativou ou não: ");
+		System.out.println("digite o seu nome: ");
 		
 		Scanner scanner = new Scanner(System.in);
 		String nome = scanner.next();
@@ -20,10 +21,31 @@ public class ProgramEnumTeste {
 		System.out.println("digite a nota dele");
 		Integer nota = scanner.nextInt();
 		
-		Aluno aluno = new Aluno(nome, nota, AlunoEnumSimples.valueOf(status.toUpperCase()));
+		//AlunoEnumSimples alunn = AlunoEnumSimples.APROVADO; acesso direto
+		//Aluno aluno = new Aluno(nome, nota, AlunoEnumSimples.valueOf(status.toUpperCase()));
 		
-		System.out.println(aluno.toString());
-	
+		//AlunoDiversosValores alunoDiversos = AlunoDiversosValores.APROVADO;
+		
+		if(status.toUpperCase().equals(AlunoDiversosValores.APROVADO.getStatus())) {
+			Aluno A1 = new Aluno(nome, nota, AlunoDiversosValores.APROVADO);
+			System.out.println("sua nota é: " + AlunoDiversosValores.APROVADO.getNota());
+		}else {
+			Aluno A1 = new Aluno(nome, nota, AlunoDiversosValores.REPROVADO);
+			System.out.println("sua nota é: " + AlunoDiversosValores.REPROVADO.getNota());
+
+		}
+		
+		//System.out.println(aluno.toString());
+		
+		
+		for(AlunoDiversosValores alun : AlunoDiversosValores.values()) {
+			if(status.toUpperCase().equals(status)) {
+				System.out.println("sua nota foi: " + alun.getNota());
+			}
+		} 
+
+			
+			
 	}
 
 }
