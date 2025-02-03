37147b2da71f4dacb769ca217c16ea98d224fe01
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2025-02-03 15:16:55 -0300

    working main

--- course/src/application/ProgramEmployee.java
+++ course/src/application/ProgramEmployee.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Scanner;
 
 import entities.Employee;
+import entities.OutsourcededEmployee;
 
 public class ProgramEmployee {
 
@@ -20,17 +21,37 @@ public class ProgramEmployee {
 		
 		for(int i = 1 ; i <= n; i ++) {
 			
-			System.out.println("o funcionario " + i + "é outsourced? SIM/NÃO?");
+			System.out.println("o funcionario " + i + "- é outsourced? SIM/NÃO?");
 			
 			String resposta = scan.nextLine().toUpperCase();
 			
 			System.out.println("employees name: ");
 			String name = scan.nextLine();
+			scan.nextLine();
 			
+			System.out.println("hours: ");
+			Integer hours = scan.nextInt();
+			scan.nextLine();
+			
+			System.out.println("value per hour: ");
+			Double valuePerHour = scan.nextDouble();
+			
+			if(resposta.equals("SIM")) {
+				System.out.println("additional charge: ");
+				Double additionalCharge = scan.nextDouble();
+				
+				list.add(new OutsourcededEmployee(name, hours, valuePerHour, additionalCharge));
+			}else {
+				list.add(new Employee(name, hours, valuePerHour));
+			}	
 		}
 		
+		System.out.println("-------------");
+		System.out.println("PAYMENTS: ");
 		
+		for(Employee emp : list) {
+			System.out.println(emp.getName() + " " + "$" + emp.payment());
+		}
 		scan.close();
 	}
-
 }
