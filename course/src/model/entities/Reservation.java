01c9cdca0bc775060338ac543f37992b24700e15
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2025-02-21 17:14:54 -0300

    segregation of the logic, verification

--- course/src/model/entities/Reservation.java
+++ course/src/model/entities/Reservation.java
@@ -39,9 +39,19 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 	
-	public void updateDates(Date checkIn, Date checkOut) {
+	public String updateDates(Date checkIn, Date checkOut) {
+		
+		Date now = new Date();
+		if(checkIn.before(now) || checkOut.before(now)) {
+			return "error , dates must be in the futre";
+		}if (!checkOut.after(checkIn)) {
+			return "error in reservation, check ou must be after";
+
+		}
+		
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+		return null;
 	}
 	
 	@Override
