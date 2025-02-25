fd99838e8b2d0c56dff4c6c1e0398a7c783f291d
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2025-02-25 18:36:59 -0300

    personal exce

---
+++ course/src/model/exceptions/AccountException.java
@@ -0,0 +1,10 @@
+package model.exceptions;
+
+public class AccountException extends Exception {
+	private static final long serialVersionUID = 1L;
+	
+	public AccountException(String msg) {
+		super(msg);
+	}
+
+}
